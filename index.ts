9c71efdcaa31a3a38cb4d99f217b53c52359aba9
Julian Haeger <julian.haeger@duecourse.com>
2017-06-07 09:04:27 +0100

    clean out all swagger specific stuff

--- index.ts
+++ index.ts
@@ -1,11 +1,3 @@
-import * as getStdIn from 'get-stdin';
-import * as swagger from 'swagger-client';
-import {buildStatementsForClient} from './src/swaggerTypeExtractor';
 import * as codeGeneration from './src/codeGeneration';
-
-const interfaceName = process.argv[2];
-
-getStdIn()
-	.then(spec => swagger({spec: JSON.parse(spec), swaggerOps: {}}))
-	.then(client => codeGeneration.renderStatementsByGroup(buildStatementsForClient(client, interfaceName)))
-	.then(console.log);
+import * as intermediateRepresentation from './src/intermediateRepresentation';
+export {codeGeneration, intermediateRepresentation};
